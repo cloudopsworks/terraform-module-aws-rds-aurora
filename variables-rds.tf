a2f8f0207c77e96885ef59d81a2e5cc514eb29e6
Cristian Beraha <cristian@cloudops.works>
2024-10-02 12:21:06 -0300

    chore: document

--- variables-rds.tf
+++ variables-rds.tf
@@ -4,18 +4,60 @@
 #            Distributed Under Apache v2.0 License
 #
 
+## YAML Input Format
+# settings:
+#   # Cluster general
+#   name_prefix: "mydb"
+#   database_name: "mydb"
+#   master_username: "admin"
+#   engine_type: "aurora-postgresql"
+#   engine_version: "10.7"
+#   availability_zones: ["us-east-1a", "us-east-1b"]
+#   port: 5432
+#   apply_immediately: true
+#   encryption:
+#     enabled: true
+#     kms_key_id: "arn:aws:kms:us-east-1:123456789012:key/12345678-1234-1234-1234-123456789012"
+#   backup:
+#     retention_period: 7
+#     window: "01:00-03:00"
+#     copy_tags: true
+#   deletion_protection: true
+#   allow_upgrade: true
+#   # Instance specific
+#   replicas: 2
+#   instance_size: "db.r5.large"
+
 variable "settings" {
   description = "Settings for RDS instance"
   type        = any
   default     = {}
 }
 
+## YAML Input Format
+# vpc:
+#   vpc_id: "vpc-12345678901234"
+#   subnet_group: "database_subnet_group_name"
+#   subnet_ids:
+#     - "subnet-abcdef123456789"
+#     - "subnet-abcdef123456781"
+#     - "subnet-abcdef123456782"
 variable "vpc" {
   description = "VPC for RDS instance"
   type        = any
   default     = {}
 }
 
+## YAML Input Format
+# security_groups:
+#   create: true
+#   name: sg-rds # Name of the security group if create = false
+#   allow_cidrs:
+#     - "1.2.3.4/32"
+#     - "1.2.0.0/16"
+#   allow_security_groups:
+#     - "sg-name-123456"
+#     - "sg-name-abcdef"
 variable "security_groups" {
   description = "Security groups for RDS instance"
   type        = any
