1cdbc9e5b941f4c50708fa2a2e024734ef2fa3d1
Cristian Beraha <cristian@cloudops.works>
2025-04-13 17:03:08 -0300

    Add password rotation support for RDS Secrets Manager
    Added support for password rotation via AWS Secrets Manager, including configuration for rotation Lambda functions and optional KMS encryption for secrets. Updated RDS cluster to handle managed password rotation with conditional secret management and encrypted master user secrets. Enhanced flexibility and security for managing RDS credentials.

--- main.tf
+++ main.tf
@@ -27,28 +27,29 @@ resource "random_string" "final_snapshot" {
 
 # Provisions RDS instance only if rds_provision=true
 resource "aws_rds_cluster" "this" {
-  cluster_identifier          = "rds-${var.settings.name_prefix}-${local.system_name}"
-  engine                      = var.settings.engine_type
-  engine_version              = var.settings.engine_version
-  global_cluster_identifier   = try(var.settings.global_cluster.create, false) ? aws_rds_global_cluster.this[0].id : try(var.settings.global_cluster.id, null)
-  availability_zones          = var.settings.availability_zones
-  database_name               = local.db_name
-  master_username             = local.master_user
-  master_password             = try(var.settings.managed_password_rotation, false) ? null : random_password.randompass[0].result
-  manage_master_user_password = try(var.settings.managed_password_rotation, false) ? true : null
-  backup_retention_period     = try(var.settings.backup.retention_period, 5)
-  preferred_backup_window     = try(var.settings.backup.window, "01:00-03:00")
-  copy_tags_to_snapshot       = try(var.settings.backup.copy_tags, true)
-  apply_immediately           = try(var.settings.apply_immediately, true)
-  vpc_security_group_ids      = local.security_group_ids
-  storage_encrypted           = try(var.settings.storage.encryption.enabled, false)
-  db_subnet_group_name        = var.vpc.subnet_group
-  kms_key_id                  = try(var.settings.storage.encryption.kms_key_id, null)
-  port                        = local.rds_port
-  final_snapshot_identifier   = "rds-${var.settings.name_prefix}-${local.system_name}-cluster-final-snap-${random_string.final_snapshot.result}"
-  deletion_protection         = try(var.settings.deletion_protection, true)
-  allow_major_version_upgrade = try(var.settings.allow_upgrade, true)
-  tags                        = local.all_tags
+  cluster_identifier            = "rds-${var.settings.name_prefix}-${local.system_name}"
+  engine                        = var.settings.engine_type
+  engine_version                = var.settings.engine_version
+  global_cluster_identifier     = try(var.settings.global_cluster.create, false) ? aws_rds_global_cluster.this[0].id : try(var.settings.global_cluster.id, null)
+  availability_zones            = var.settings.availability_zones
+  database_name                 = local.db_name
+  master_username               = local.master_user
+  master_password               = try(var.settings.managed_password_rotation, false) ? null : random_password.randompass[0].result
+  manage_master_user_password   = try(var.settings.managed_password_rotation, false) ? true : null
+  master_user_secret_kms_key_id = try(var.settings.managed_password_rotation, false) ? try(var.settings.password_secret_kms_key_id, null) : null
+  backup_retention_period       = try(var.settings.backup.retention_period, 5)
+  preferred_backup_window       = try(var.settings.backup.window, "01:00-03:00")
+  copy_tags_to_snapshot         = try(var.settings.backup.copy_tags, true)
+  apply_immediately             = try(var.settings.apply_immediately, true)
+  vpc_security_group_ids        = local.security_group_ids
+  storage_encrypted             = try(var.settings.storage.encryption.enabled, false)
+  db_subnet_group_name          = var.vpc.subnet_group
+  kms_key_id                    = try(var.settings.storage.encryption.kms_key_id, null)
+  port                          = local.rds_port
+  final_snapshot_identifier     = "rds-${var.settings.name_prefix}-${local.system_name}-cluster-final-snap-${random_string.final_snapshot.result}"
+  deletion_protection           = try(var.settings.deletion_protection, true)
+  allow_major_version_upgrade   = try(var.settings.allow_upgrade, true)
+  tags                          = local.all_tags
 }
 
 resource "aws_rds_cluster_instance" "this" {
