4f2f737c0f79899583417e05c57349b693edfcd4
Cristian Beraha <cristian@cloudops.works>
2025-12-03 12:19:49 -0300

    PIP-47 chore: Update master password logic to handle migration-enabled configurations in Terraform

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_rds_cluster" "this" {
   availability_zones                  = var.settings.availability_zones
   database_name                       = local.db_name
   master_username                     = local.master_user
-  master_password                     = try(var.settings.managed_password, false) ? null : random_password.randompass[0].result
+  master_password                     = try(var.settings.managed_password, false) ? null : (!try(var.settings.migration.enabled, false) ? random_password.randompass[0].result : null)
   manage_master_user_password         = try(var.settings.managed_password, false) ? true : null
   master_user_secret_kms_key_id       = try(var.settings.managed_password_rotation, false) ? try(var.settings.password_secret_kms_key_id, null) : null
   backup_retention_period             = try(var.settings.backup.retention_period, 5)
