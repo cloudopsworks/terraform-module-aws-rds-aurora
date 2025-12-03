02d6e5f2bc628eac3849df25c98a7e832042c356
Cristian Beraha <cristian@cloudops.works>
2025-12-03 16:36:22 -0300

    PIP-47 chore: Refine custom endpoint logic to ensure unique keys by mapping endpoint name in Terraform configuration

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ resource "aws_rds_cluster_instance" "this" {
 }
 
 resource "aws_rds_cluster_endpoint" "this" {
-  for_each                    = try(var.settings.custom_endpoints, [])
+  for_each                    = { for endpoint in try(var.settings.custom_endpoints, []) : endpoint.name => endpoint }
   cluster_identifier          = aws_rds_cluster.this.id
   cluster_endpoint_identifier = each.value.name
   custom_endpoint_type        = upper(each.value.type)
